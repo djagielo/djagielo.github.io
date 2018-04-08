1cea951cf5bbff6a6a9f430e1015a065962907a9
Dariusz Jagieło <dariusz.jagielo@gmail.com>
2018-04-08 15:54:18 +0200

    automatic generator for tags in gulpfile, simple tag cloud

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var cp          = require('child_process');
+var _ = require('lodash');
+var fs = require('fs');
 
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build --incremental'
@@ -38,11 +40,28 @@ gulp.task('browser-sync', ['jekyll-build'], function() {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/**', 'assets/**/*.css', 'assets/**/*.scss'], ['jekyll-rebuild']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/**', 'assets/**/*.css', 'assets/**/*.scss'], ['generate-tags', 'jekyll-rebuild', 'generate-tags']);
+});
+
+gulp.task('generate-tags', function() {
+    var tags = require('./_site/tags/tags.json');
+    _.forEach(tags.tags, tag => {
+        if (!fs.existsSync(`tags/${tag}`)) {
+            fs.mkdirSync(`tags/${tag}`);
+            var stream = fs.createWriteStream(`tags/${tag}/index.md`);
+            stream.once('open', function(fd) {
+                stream.write('---\n');
+                stream.write('layout: tag_index\n');
+                stream.write(`tag: ${tag}\n`);
+                stream.write('---');
+                stream.end();
+            });
+        }
+    });
 });
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['generate-tags', 'browser-sync', 'watch', ]);
